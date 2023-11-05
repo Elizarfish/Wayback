e2dd688c2b2a7e89d1d0308483061b97d828a5df
api_0 <47054974+Elizarfish@users.noreply.github.com>
2023-11-05 10:53:08 +0300

    Update wayback.py
    In the fetch_robots_txt function:
    
    The status code check that used != 200 has been replaced with == 200.
    A try/except block has been added to catch exceptions that may occur during HTTP requests or file read/write operations.
    Print statements have been added to indicate success and error states.
    In the worker function:
    
    A call to domain_queue.task_done() has been added after processing each domain to properly signal the completion of tasks in the queue.
    A message indicating the completion of domain processing has been added.
    In the main function:
    
    Results of executor.submit() are saved to a list for subsequent invocation of future.result(), which allows raising an exception if one occurred within a thread.

--- wayback.py
+++ wayback.py
@@ -7,38 +7,46 @@ from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
 
 def fetch_robots_txt(domain, output_file):
-    url = f"http://web.archive.org/cdx/search/cdx?url={domain}/robots.txt&output=json"
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-        return
+    try:
+        url = f"http://web.archive.org/cdx/search/cdx?url={domain}/robots.txt&output=json"
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            print(f"Error fetching robots.txt for {domain}: {response.status_code}")
+            return
 
-    records = response.json()
-    unique_paths = set()
+        records = response.json()
+        unique_paths = set()
 
-    if len(records) >= 1:
-        for record in records[1:]:
-            timestamp = record[1]
-            wayback_url = f"http://web.archive.org/web/{timestamp}if_/{record[2]}"
-            resp = requests.get(wayback_url)
+        if len(records) >= 1:
+            for record in records[1:]:
+                timestamp = record[1]
+                wayback_url = f"http://web.archive.org/web/{timestamp}if_/{record[2]}"
+                resp = requests.get(wayback_url)
 
-            if resp.status_code >= 200 and resp.status_code <= 200:
-                body = resp.text
-                path_pattern = re.compile(r"/[\w\-./?=]*")
-                path_matches = path_pattern.findall(body)
+                if resp.status_code == 200:
+                    body = resp.text
+                    path_pattern = re.compile(r"/[\w\-./?=]*")
+                    path_matches = path_pattern.findall(body)
 
-                for path in path_matches:
-                    unique_paths.add(path)
+                    for path in path_matches:
+                        unique_paths.add(path)
 
-    with open(output_file, "a") as f:
-        for path in unique_paths:
-            f.write(f"{domain} {path}\n")
+        with open(output_file, "a") as f:
+            for path in unique_paths:
+                f.write(f"{domain} {path}\n")
+
+        print(f"Successfully fetched and wrote paths for {domain}")
+
+    except Exception as e:
+        print(f"An error occurred while processing {domain}: {e}")
 
 def worker(domain_queue, output_file):
     while not domain_queue.empty():
         domain = domain_queue.get()
         fetch_robots_txt(domain, output_file)
+        domain_queue.task_done()
+        print(f"Finished processing {domain}")
 
 def main():
     parser = argparse.ArgumentParser(description="Wayback Machine robots.txt fetcher")
@@ -53,8 +61,11 @@ def main():
             domain_queue.put(line.strip())
 
     with ThreadPoolExecutor(max_workers=20) as executor:
-        for _ in range(20):
-            executor.submit(worker, domain_queue, args.output)
+        futures = [executor.submit(worker, domain_queue, args.output) for _ in range(20)]
+        for future in futures:
+            future.result()  # This will raise an exception if any occurred within a thread
+
+    print("All work has been processed.")
 
 if __name__ == "__main__":
     main()
